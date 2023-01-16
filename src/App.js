fee28b2b2b67e08bb5903ebd7442985409dafcb7
Torben <tschlawe@gmail.com>
2023-01-16 11:58:29 +0100

    Check for mobile device

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from "react";
 import "./App.css";
 import { Outlet } from "react-router-dom";
+import { isMobile } from "react-device-detect";
 
 function App() {
-  return (
-    <div className="grid place-items-center h-screen bg-gray-400">
-      <div className="flex flex-row  bg-[#141b26]  rounded-lg shadow-md w-max h-3/4">
-        <img
-          className="object-contain rounded-tl-lg rounded-bl-lg h-full"
-          src="images/side-banner.png"
-          alt=""
-        ></img>
-        <Outlet />
+  if (isMobile) {
+    return <h1>Please view in a browser</h1>;
+  } else
+    return (
+      <div className="grid place-items-center h-screen bg-gray-400">
+        <div className="flex flex-row  bg-[#141b26]  rounded-lg shadow-md w-max h-3/4">
+          <img
+            className="object-contain rounded-tl-lg rounded-bl-lg h-full"
+            src="images/side-banner.png"
+            alt=""
+          ></img>
+          <Outlet />
+        </div>
       </div>
-    </div>
-  );
+    );
 }
 
 export default App;
